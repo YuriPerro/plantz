6504e9091a1da72d29e039c7d17b791c398f1212
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2021-04-26 20:35:42 -0300

    Criando plant select

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/core';
 import {
     SafeAreaView,
     StyleSheet,
     Text,
-    TextInput,
     View,
-    KeyboardAvoidingView,
-    Platform,
 }
     from 'react-native';
 import { Button } from '../components/Button';
@@ -14,6 +12,13 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation() {
+
+    const navigation = useNavigation();
+
+    function handleMoveOn() {
+        navigation.navigate('PlantSelect');
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
@@ -30,7 +35,7 @@ export function Confirmation() {
                 </Text>
 
                 <View style={styles.footer}>
-                    <Button title="Começar" />
+                    <Button onPress={handleMoveOn} title="Começar" />
                 </View>
             </View>
         </SafeAreaView>
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import colors from '../styles/colors';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import colors from '../styles/colors';
 
 const stackRoutes = createStackNavigator();
 
@@ -29,6 +32,11 @@ const AppRoutes: React.FC = () => (
         <stackRoutes.Screen
             name="Confirmation"
             component={Confirmation}
+        />
+
+        <stackRoutes.Screen
+            name="PlantSelect"
+            component={PlantSelect}
         />
 
     </stackRoutes.Navigator>
