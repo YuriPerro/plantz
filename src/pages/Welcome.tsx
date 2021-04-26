787810011cede16f093086004fb3219ff3a58700
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2021-04-26 01:45:56 -0300

    Criando primeiras navegaçoes

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -15,13 +15,14 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 import { Entypo } from "@expo/vector-icons"
+import { useNavigation } from '@react-navigation/core';
 
 export function Welcome() {
 
-    const [visible, setVisible] = useState(false);
+    const navigation = useNavigation();
 
-    function handleVisibility() {
-        setVisible((prev) => !prev);
+    function handleStart() {
+        navigation.navigate('UserIdentification')
     }
 
     return (
@@ -46,6 +47,7 @@ export function Welcome() {
                 <TouchableOpacity
                     style={styles.button}
                     activeOpacity={0.7}
+                    onPress={handleStart}
                 >
                     <Entypo
                         name="chevron-right"
